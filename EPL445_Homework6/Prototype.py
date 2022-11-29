b5be6e87f386be85945bd82af60c803a81c80f89
eeliad0 <eeliad02@ucy.ac.cy>
2022-11-29 15:48:27 +0200

    project with sound - missing face detection and recording

--- EPL445_Homework6/Prototype.py
+++ EPL445_Homework6/Prototype.py
@@ -116,27 +116,22 @@ class Ui_Form(object):
         self.image_matching()
 
     def image_matching(self):
-
         h, w = proto.shape
         # All the 6 methods for comparison in a list
         methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR', 'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF',
                    'cv2.TM_SQDIFF_NORMED']
         for meth in methods:
-
             img = img2.copy()
             # Enumerate the template matching operation
             method = eval(meth)
-
             # Apply template matching
             res = cv2.matchTemplate(img, proto, method)
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
             # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take the minimum
             if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
                 top_left = min_loc
             else:
                 top_left = max_loc
-
             bottom_right = (top_left[0] + w, top_left[1] + h)
             cv2.rectangle(img, top_left, bottom_right, 255, 2)
 
